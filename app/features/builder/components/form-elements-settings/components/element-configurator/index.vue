3d79b120d56b4392e0d866eb1bdba46ac3dd6f52
pinyu <oa51360@gmail.com>
2025-10-29 22:47:29 +0800

    feat(builder module): 元素設定元件初始化

---
+++ app/features/builder/components/form-elements-settings/components/element-configurator/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>element-configurator</div>
+    <div>基本設定</div>
+    
+</template>
--- app/features/builder/components/form-elements-settings/form-elements-settings.vue
+++ app/features/builder/components/form-elements-settings/form-elements-settings.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import DropBox from './components/drop-box.vue';
+import ElementConfigurator from './components/element-configurator/index.vue';
+
 import type { FormConfig } from '../../types/form-config'
 
 const formConfig = ref<FormConfig>({
@@ -16,8 +18,14 @@ const formConfig = ref<FormConfig>({
 </script>
 
 <template>
-    <div class="p-3">
-      settings
+    <div class="p-3 flex-col gap-4">
       <DropBox />
+      <div
+        class="border-2"
+        v-for="(_, index) in formConfig.render"
+        :key="index"
+      >
+        <ElementConfigurator v-model:element="formConfig.render[index]"/>
+      </div>
     </div>
 </template>
