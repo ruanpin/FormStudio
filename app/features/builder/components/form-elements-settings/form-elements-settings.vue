e753ebe908ec75f033a9dfe304bdeb97443385f1
pinyu <oa51360@gmail.com>
2025-10-29 22:27:32 +0800

    feat(builder module): 初始化formConfig & 類型

--- app/features/builder/components/form-elements-settings/form-elements-settings.vue
+++ app/features/builder/components/form-elements-settings/form-elements-settings.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
 import DropBox from './components/drop-box.vue';
+import type { FormConfig } from '../../types/form-config'
+
+const formConfig = ref<FormConfig>({
+  render: [],
+  submit: {
+    method: "POST",
+    urls: [
+      { url: "firstUrl" },
+    ],
+    ask: false,
+    payloadType: "form-data",
+  }
+})
 </script>
 
 <template>
