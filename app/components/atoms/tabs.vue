1c95a647a1a0f922f64d59e01a8aafdc384bd04d
pinyu <oa51360@gmail.com>
2025-11-30 18:35:41 +0800

    feat(component): 共用元件tabs

---
+++ app/components/atoms/tabs.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+
+interface Tab {
+    label: string;
+    value: string;
+}
+
+defineProps<{
+    tabs: Tab[];
+    defaultTab: string;
+}>()
+</script>
+
+<template>
+    <Tabs :default-value="defaultTab">
+        <TabsList>
+            <TabsTrigger
+                v-for="tab in tabs"
+                :key="tab.value"
+                :value="tab.value"
+            >
+                {{ tab.label }}
+            </TabsTrigger>
+        </TabsList>
+        <TabsContent
+            v-for="tab in tabs"
+            :key="tab.value"
+            :value="tab.value"
+        >
+            <slot :name="tab.value" />
+        </TabsContent>
+    </Tabs>
+</template>
