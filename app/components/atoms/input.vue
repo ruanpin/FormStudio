1866e9ac68d5f8d6866773d148d11c97565adfb5
pinyu <oa51360@gmail.com>
2025-12-07 21:56:27 +0800

    feat(Input component): 支援append slot 通常用於按鈕

--- app/components/atoms/input.vue
+++ app/components/atoms/input.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
 import { Input } from '@/components/ui/input';
+import { cn } from "@/lib/utils"
 </script>
 
 <template>
-    <Input v-bind="$attrs"/>
+    <div class="relative">
+        <Input v-bind="$attrs" :class="cn('pr-10', $attrs.class as string)" />
+        <div v-if="$slots.append" class="absolute inset-y-0 right-0 flex items-center">
+            <slot name="append"></slot>
+        </div>
+        <slot v-else></slot>
+    </div>
 </template>
